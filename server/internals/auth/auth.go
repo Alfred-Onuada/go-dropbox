098cf8262e6ce43036beffd1935b2d892a8e4a43
shepherrrd <shefkid2@gmail.com>
2024-06-25 17:41:02 +0100

    minor response fix

--- server/internals/auth/auth.go
+++ server/internals/auth/auth.go
@@ -49,6 +49,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	err := decoder.Decode(&data)
 	if err != nil {
 		helpers.JSONResponse(w,false,"An Error Occured",http.StatusInternalServerError,nil)
+		return
 	}
    
 	username := data["username"]
@@ -57,17 +58,20 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	var user types.User
 	if err := DB.Where("username = ?", username).First(&user); err == nil {
 		helpers.JSONResponse(w,false,"Invalid Credentials",http.StatusUnauthorized,nil)
+		return
 
 	}
 	validpassword := checkPasswordHash(password,user.Password)
 	
 	if !validpassword  {
 		helpers.JSONResponse(w,false,"Invalid Credentials",http.StatusUnauthorized,nil)
+		return
 
 	} 
 	 token,err := GenerateJWT(user.Username)
 	 if err !=nil {
 		helpers.JSONResponse(w,false ,"Something Went Wrong , ask your ex",http.StatusInternalServerError,nil)
+		return
 	 }
 		jsonresp := map[string]interface{}{
 			"item": user.Item,
@@ -75,6 +79,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 			"token": token,
 		}
 		helpers.JSONResponse(w,true,"Login Successfull",http.StatusOK,jsonresp)
+		return
 		
 	}
 	
@@ -88,6 +93,7 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	err := decoder.Decode(&data)
 	if err != nil {
 		helpers.JSONResponse(w,false,"There was a problem processing the JSON",http.StatusInternalServerError,nil)
+		return
 
 	}
 	
@@ -97,18 +103,20 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	 var existinguser types.User
 	if err := DB.Where("username = ?" , username).First(&existinguser).Error; err==nil {
 		helpers.JSONResponse(w,false,"Username Is Taken",http.StatusBadRequest,nil)
+		return
 
 	}
 	 
 	 curDir,err := os.Getwd() // change later to s3 
 	 if err != nil {
 		helpers.JSONResponse(w,false,"Unable to get the directory",http.StatusInternalServerError,nil)
-
+		return
 	}
 	 userDir := curDir + "/users/" + username
 	 hashpassword,err := hashPassword(password)
 	 if err != nil {
 		helpers.JSONResponse(w,false,"An Unknown error occured",http.StatusInternalServerError,nil)
+		return
 
 	 }
 	 user := types.User{ 
@@ -125,9 +133,11 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	 user.Item = &item
 	 if err := DB.Create(&user).Error; err != nil {
 		helpers.JSONResponse(w,false,"There was a problem processing your request",http.StatusInternalServerError,nil)
+		return
 
 	}
 	helpers.JSONResponse(w,true,"Login Successfull",http.StatusOK,nil)
+	return
 
 	
 }
--- server/internals/helpers/helpers.go
+++ server/internals/helpers/helpers.go
@@ -17,6 +17,6 @@ func JSONResponse(w http.ResponseWriter, status bool, message string, statuscode
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statuscode)
 	json.NewEncoder(w).Encode(response)
-	return
+	
 }
 
